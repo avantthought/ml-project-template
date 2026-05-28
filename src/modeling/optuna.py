0706d3bf525f57e68174d3a25d4dd4383dc00e4e
CMMartin <avantthought@gmail.com>
2026-05-28 12:41:29 -0500

    finish optuna.py

---
+++ src/modeling/optuna.py
@@ -0,0 +1,81 @@
+"""Helper functions for hyperparameter optimization using the optuna library."""
+
+from functools import partial
+
+import optuna
+import pandas as pd
+from sklearn.model_selection import cross_validate
+
+
+def train_pipeline_with_optuna(pipeline, x, y, estimator_name, cv_strategy, cv_scoring_dict, param_func,
+                               iterations, save_scores=False, save_path=None):
+    """
+    Trains and optimizes hyperparameters of a pipeline using the optuna. Returns the fitted pipeline set with the
+        best parameters determined from the optimization.
+    Each trial of the optimization uses mean cross validation scores of the first metric in cv_scoring_dict. A csv
+        of all the trials with scores can optionally be saved. The trials are sorted from best to worst.
+
+    :param sklearn.pipeline.Pipeline pipeline: pipeline to optimize
+    :param pd.DataFrame x: features dataframe
+    :param pd.Series y: target series
+    :param str estimator_name: the name of the final estimator in the pipeline
+    :param Union[int, callable] cv_strategy: cross validation strategy
+    :param Dict[str, str] cv_scoring_dict: dictionary of scoring metrics to use during cross-validation
+    :param function param_func: parameter space (wrapped in a function) to search for optimal hyperparameters
+        using optuna; the function needs "trial" as the only parameter with the (dictionary) parameter space returned
+    :param int iterations: number of trials to run the hyperparameter optimization
+    :param bool save_scores: whether to save the scores csv or not; default is False
+    :param Union[pathlib.Path, None] save_path: path (from project root) where to save scores csv
+        if truthy; default is None
+    :return: fitted pipeline set with the best hyperparameters from the optimization
+    :rtype: sklearn.pipeline.Pipeline
+    """
+    cv_scores_df = pd.DataFrame()
+
+    def _make_cv_score_row_df(scores_array, name):
+        cv_score_row_df = pd.DataFrame(scores_array).T
+        cv_score_row_df.columns = 'fold_' + cv_score_row_df.columns.astype(str) + f'_{name}'
+        cv_score_row_df[f'mean_{name}'] = cv_score_row_df.mean(axis=1)
+        cv_score_row_df[f'std_{name}'] = cv_score_row_df.drop(columns=[f'mean_{name}']).std(axis=1)
+        return cv_score_row_df
+
+    def _objective_partial(trial, pipeline, x, y, cv_strategy, cv_scoring_dict, param_func):
+        params = param_func(trial)
+        estimator_param_names = pipeline.get_params().keys()
+        new_mapping = {g: params[f] for f in params.keys() for g in estimator_param_names if g.endswith(f)}
+        pipeline.set_params(**new_mapping)
+        scores_dict = cross_validate(pipeline, x, y, cv=cv_strategy, scoring=cv_scoring_dict, n_jobs=-1)
+        scores_df_list = []
+        for score_key in list(scores_dict.keys())[2:]:
+            name = score_key.split('_', 1)[1]
+            score_df = _make_cv_score_row_df(scores_dict[score_key], name)
+            scores_df_list.append(score_df)
+        trial_cv_scores_row_df = pd.concat(scores_df_list, axis=1)
+        temp_params_df = pd.DataFrame(params, index=[0])
+        trial_cv_scores_row_df = pd.concat([temp_params_df, trial_cv_scores_row_df], axis=1)
+        nonlocal cv_scores_df
+        trial_cv_scores_row_df.index = [len(cv_scores_df)]
+        mean_optimization_score = trial_cv_scores_row_df[
+            f'mean_{list(cv_scoring_dict.keys())[0]}'
+        ][len(cv_scores_df)]
+        cv_scores_df = pd.concat([cv_scores_df, trial_cv_scores_row_df], axis=0)
+        return mean_optimization_score
+
+    study = optuna.create_study(direction='maximize')
+    objective = partial(_objective_partial, pipeline=pipeline, x=x, y=y,
+                        cv_strategy=cv_strategy, cv_scoring_dict=cv_scoring_dict, param_func=param_func)
+    study.optimize(objective, n_trials=iterations)
+    params = study.best_params
+    estimator_param_names = pipeline.get_params().keys()
+    best_params = {g: params[f] for f in params.keys() for g in estimator_param_names if g.endswith(f)}
+
+    cv_scores_df = cv_scores_df.sort_values(by=[f'mean_{list(cv_scoring_dict.keys())[0]}'], ascending=False)
+    cv_scores_df = cv_scores_df.reset_index(drop=True)
+    cv_scores_df = cv_scores_df.reset_index()
+    cv_scores_df = cv_scores_df.rename(columns={'index': 'ranking'})
+    pipeline.set_params(**best_params)
+    pipeline.fit(x, y)
+    if save_scores:
+        if save_path is not None:
+            cv_scores_df.to_csv(f'{save_path}/{estimator_name}_optuna_cv_scores.csv')
+    return pipeline
--- src/modeling/train.py
+++ src/modeling/train.py
@@ -1,7 +1,5 @@
 """Model training script."""
 
-from pprint import pprint
-
 from auto_shap import produce_shap_values_and_summary_plots
 import joblib
 import pandas as pd
@@ -15,6 +13,7 @@ from src.config.config import (PROCESSED_DATA_DIR, RAW_DATA_DIR, LOAD_CACHED_DAT
 from src.data.build import build
 from src.config.estimators import MODEL_TRAINING_LIST
 from src.modeling.evaluate import evaluate_model
+from src.modeling.optuna import train_pipeline_with_optuna
 from src.modeling.pipeline import create_pipeline, pipeline_preprocessor_model_splitter
 from src.modeling.process import (create_target, create_x_y_dataframes, create_test_train_splits,
                                   determine_iterations, determine_shap_sampling)
@@ -22,7 +21,7 @@ from src.utils.utils import make_dir, create_datetime_id
 
 
 def train_and_evaluate(training_path, model, model_name, param_func, iterations, x_train, x_test, y_train, y_test,
-                       non_modeling_fields_to_drop, cv_splits, cv_scoring_dict, decision_boundary=0.5,
+                       non_modeling_fields_to_drop, cv_strategy, cv_scoring_dict, decision_boundary=0.5,
                        shap_sample_size=None, shap_on_both_test_and_train=False, also_save_model_in_root=False):
     """
     Model training, evaluation, and explanation function for a single model. The trained model and all diagnostics
@@ -34,13 +33,13 @@ def train_and_evaluate(training_path, model, model_name, param_func, iterations,
     :param str model_name: string name of the model
     :param function param_func: parameter space (wrapped in a function) to search for optimal hyperparameters
         using optuna; the function needs "trial" as the only parameter with the (dictionary) parameter space returned
-    :param int iterations: max number of trials to run the optimization
+    :param int iterations: number of trials to run the hyperparameter optimization
     :param pd.DataFrame x_train: x_train
     :param pd.DataFrame x_test: x_test
     :param pd.Series y_train: y_train
     :param pd.Series y_test: y_test
     :param List[str] non_modeling_fields_to_drop: list of columns to drop for modeling in pipeline
-    :param Union[int, callable] cv_splits: cross validation strategy
+    :param Union[int, callable] cv_strategy: cross validation strategy
     :param Dict[str, str] cv_scoring_dict: dictionary of scoring metrics to use during cross-validation
     :param float decision_boundary: threshold for classifying positive class; default is 0.5
     :param Union[int, None] shap_sample_size: sample size for SHAP value computation; default is None (no limit)
@@ -55,39 +54,61 @@ def train_and_evaluate(training_path, model, model_name, param_func, iterations,
     make_dir(model_path)
     pipeline = create_pipeline(model=model, fields_to_drop=non_modeling_fields_to_drop)
     print(model_name)
-    # objective = Objective(pipeline, x_train, y_train, cv=cv_splits, scoring=cv_scoring_dict)
-    # hopt = Hyperoptimizer(objective, param_grid, max_evals=iterations).search()
-    # pprint(hopt.results)
-    # hyperopt_results_df = create_hyperopt_scores_df(hopt.trials, model_name)
-    # hyperopt_results_df.to_csv(f'{model_path}/{model_name}_hyperopt_cv_scores.csv')
-    # pipeline = pipeline.set_params(**hopt.best_params)
-    pipeline.fit(x_train, y_train)
+    pipeline = train_pipeline_with_optuna(
+        pipeline=pipeline,
+        x=x_train,
+        y=y_train,
+        estimator_name=model_name,
+        cv_strategy=cv_strategy,
+        cv_scoring_dict=cv_scoring_dict,
+        param_func=param_func,
+        iterations=iterations,
+        save_scores=True,
+        save_path=model_path
+    )
     joblib.dump(pipeline, f'{model_path}/{model_name}_pipeline.pkl')
     if also_save_model_in_root:
         root = model_path.parents[3]
         uid_pipeline_tuple = (training_path.stem, pipeline)
         joblib.dump(uid_pipeline_tuple, f'{root}/{model_name}_pipeline.pkl')
-    evaluate_model(pipeline=pipeline, x=x_test, y=y_test, estimator_name=model_name, results_path=model_path,
-                   decision_boundary=decision_boundary, use_cv_for_eval=False, cv=cv_splits)
-
+    evaluate_model(
+        pipeline=pipeline,
+        x=x_test,
+        y=y_test,
+        estimator_name=model_name,
+        results_path=model_path,
+        decision_boundary=decision_boundary,
+        use_cv_for_eval=False,
+        cv=cv_strategy
+    )
     x_train_sub, x_test_sub, y_train_sub, y_test_sub =(
         determine_shap_sampling(x_train, x_test, y_train, y_test, n=shap_sample_size, reset_index=True))
     shap_path = model_path / 'shap_test_set'
     make_dir(shap_path)
     x_test_preprocessed, fitted_model = pipeline_preprocessor_model_splitter(x_test_sub, pipeline)
-    produce_shap_values_and_summary_plots(model=fitted_model, x_df=x_test_preprocessed, save_path=shap_path,
-                                          file_prefix=model_name, n_jobs=4)
+    produce_shap_values_and_summary_plots(
+        model=fitted_model,
+        x_df=x_test_preprocessed,
+        save_path=shap_path,
+        file_prefix=model_name,
+        n_jobs=4
+    )
     if shap_on_both_test_and_train:
         shap_path = model_path / 'shap_train_set'
         make_dir(shap_path)
         x_train_preprocessed, fitted_model = pipeline_preprocessor_model_splitter(x_train_sub, pipeline)
-        produce_shap_values_and_summary_plots(model=fitted_model, x_df=x_train_preprocessed, save_path=shap_path,
-                                              file_prefix=model_name, n_jobs=4)
+        produce_shap_values_and_summary_plots(
+            model=fitted_model,
+            x_df=x_train_preprocessed,
+            save_path=shap_path,
+            file_prefix=model_name,
+            n_jobs=4
+        )
 
 
 def master_train(raw_path, raw_data_filename, process_path, processed_data_filename, results_path, model_training_list,
                  target_name, non_modeling_fields_to_drop,
-                 cv_splits, cv_scoring_dict, test_set_size=0.25, decision_boundary=0.5, data_limit=None,
+                 cv_strategy, cv_scoring_dict, test_set_size=0.25, decision_boundary=0.5, data_limit=None,
                  train_baseline=True, train_only_one_baseline=False, optimization_iterations_limit=None,
                  shap_on_both_test_and_train=False, shap_sample_size=None, save_production_pipeline=False,
                  production_model_name=None, load_cached=False):
@@ -101,10 +122,10 @@ def master_train(raw_path, raw_data_filename, process_path, processed_data_filen
     :param pathlib.Path results_path: path (from project root) where to save modeling results
     :param List[scr.config.estimators.model_config] model_training_list: list of 'model_config' named tuples, where
         each tuple corresponds ot a single model that has the model name, model object, hyperparameter grid function,
-        and number of optimization iterations
+        and number of hyperparameter optimization iterations
     :param str target_name: name of the target variable/column
     :param List[str] non_modeling_fields_to_drop: list of fields/columns to drop before modeling
-    :param Union[int, callable] cv_splits: cross-validation strategy
+    :param Union[int, callable] cv_strategy: cross-validation strategy
     :param Dict[str, str] cv_scoring_dict: dictionary of scoring metrics to use during cross-validation
     :param float test_set_size: proportion of the dataset to include in the test split; default is 0.25
     :param float decision_boundary: threshold for classifying positive class; default is 0.5
@@ -131,17 +152,17 @@ def master_train(raw_path, raw_data_filename, process_path, processed_data_filen
         model_training_list_adjusted = [model_training_list_adjusted[0], model_training_list_adjusted[-1]]
     if not train_baseline:
         model_training_list_adjusted = model_training_list_adjusted[:-1]
-
-    df = build(raw_path=raw_path,
-               raw_data_filename=raw_data_filename,
-               process_path=process_path,
-               processed_data_filename=processed_data_filename,
-               load_cached=load_cached)
+    df = build(
+        raw_path=raw_path,
+        raw_data_filename=raw_data_filename,
+        process_path=process_path,
+        processed_data_filename=processed_data_filename,
+        load_cached=load_cached
+    )
     df = create_target(df, target_name)
     x, y = create_x_y_dataframes(df=df, target_name=target_name, data_limit=data_limit)
     train_test_dict = create_test_train_splits(x=x, y=y, test_set_size=test_set_size)
     joblib.dump(train_test_dict, f'{training_path}/train_test_dict.pkl')
-
     for model_tuple in model_training_list_adjusted:
         iterations = determine_iterations(optimization_iterations_limit, model_tuple.iterations)
         also_save_model_in_root = False
@@ -158,7 +179,7 @@ def master_train(raw_path, raw_data_filename, process_path, processed_data_filen
             y_train=train_test_dict['y_train'],
             y_test=train_test_dict['y_test'],
             non_modeling_fields_to_drop=non_modeling_fields_to_drop,
-            cv_splits=cv_splits,
+            cv_strategy=cv_strategy,
             cv_scoring_dict=cv_scoring_dict,
             decision_boundary=decision_boundary,
             shap_sample_size=shap_sample_size,
@@ -177,7 +198,7 @@ if __name__ == '__main__':
         model_training_list=MODEL_TRAINING_LIST,
         target_name=TARGET_NAME,
         non_modeling_fields_to_drop=NON_MODELING_FIELDS_TO_DROP,
-        cv_splits=CV_SPLITS,
+        cv_strategy=CV_SPLITS,
         cv_scoring_dict=CV_SCORING_DICT,
         test_set_size=TEST_SET_SIZE,
         decision_boundary=DECISION_BOUNDARY,
